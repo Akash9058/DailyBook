6699c786292c5df6b1516cbe40947c69cc13b23a
Akash kumer <riskyadav4321@gmail.com>
2023-03-05 13:48:20 +0530

    first commit
    Signed-off-by: Akash kumer <riskyadav4321@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 1000;
+const router= require('./routes/index');
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
+app.set('view engine','ejs');
+app.set('views','./views');
  
 
+app.use('/',require('./routes/index'));
 
 app.listen(port, function (err) {
     if (err) { console.log('error', err); }
